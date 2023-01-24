16dd907b142c257919c43eca864d77d87fecf72d
lekatehdas <aki.sirkia@gmail.com>
2023-01-24 15:43:27 +0100

    Reffactored Main activity to use binding, also removed UiElements class.

--- app/src/main/java/com/example/pseudorandomgenerator/MainActivity.kt
+++ app/src/main/java/com/example/pseudorandomgenerator/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.pseudorandomgenerator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.example.pseudorandomgenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     /*
     Methods to be used:
         -Cesar cypher
@@ -17,43 +18,40 @@ class MainActivity : AppCompatActivity() {
             - To save data to the DB together with timestamp and method name.
             Pref automatically when desired amount of data has been generated.
      */
-    private lateinit var uiElements: UiElements
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val permissionHelper = PermissionHelper(this)
         permissionHelper.checkAndAskPermissions()
-
-        uiElements = UiElements(this@MainActivity)
-        uiElements.initViews()
 
         initListeners()
     }
 
     private fun initListeners() {
-        uiElements.btnTimeButton.setOnClickListener {
+        binding.btnTimeButton.setOnClickListener {
             val intent = Intent(this, TimeButtonActivity::class.java)
             startActivity(intent)
         }
 
-        uiElements.btnTimeTyping.setOnClickListener {
+        binding.btnTimeTyping.setOnClickListener {
             val intent = Intent(this, TimeTypingActivity::class.java)
             startActivity(intent)
         }
 
-        uiElements.btnWifi.setOnClickListener {
+        binding.btnWifi.setOnClickListener {
             val intent = Intent(this, WifiNoiseActivity::class.java)
             startActivity(intent)
         }
 
-        uiElements.btnCamera.setOnClickListener {
+        binding.btnCamera.setOnClickListener {
             val intent = Intent(this, CameraActivity::class.java)
             startActivity(intent)
         }
 
-        uiElements.btnMovement.setOnClickListener {
+        binding.btnMovement.setOnClickListener {
             val intent = Intent(this, MovementActivity::class.java)
             startActivity(intent)
         }
