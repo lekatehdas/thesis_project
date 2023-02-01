6d7cd8ca72b3c11733802adf340ee425d0fdac02
lekatehdas <aki.sirkia@gmail.com>
2023-02-01 22:39:01 +0100

    Speech to text now uses byteArrays as data unit. Data is scrambled by XORing the array with it self.

--- app/src/main/java/com/example/pseudorandomgenerator/SpeechToTextActivity.kt
+++ app/src/main/java/com/example/pseudorandomgenerator/SpeechToTextActivity.kt
@@ -4,15 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.converters.ByteArrayToBinaryStringConverter
 import com.example.pseudorandomgenerator.databinding.ActivitySpeechToTextBinding
 import com.example.utilities.DataSaver
 import com.example.utilities.EnvVariables
 import com.example.data_generator.SpeechDataGenerator
+import com.example.utilities.ByteArrayListXOR
+import com.example.utilities.ByteArrayProcessor
 
 class SpeechToTextActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySpeechToTextBinding
     private lateinit var speechDataGenerator: SpeechDataGenerator
-    private var generatedData = ""
+    private val processor = ByteArrayProcessor()
+    private var stringData = ByteArray(0)
+    private var audioData = ByteArray(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,31 +33,69 @@ class SpeechToTextActivity : AppCompatActivity() {
         }
     }
 
-    private fun enoughData(): Boolean {
-        return generatedData.length >= EnvVariables.DESIRED_LENGTH
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        generatedData += speechDataGenerator.onActivityResult(requestCode, resultCode, data)
+        val listOfData: List<ByteArray> =  speechDataGenerator.onActivityResult(requestCode, resultCode, data)
+
+        if (!enoughStringData()) {
+            getStringData(listOfData)
+        }
+
+        if (!enoughAudioData()) {
+            getAudioData(listOfData)
+        }
+
         updateUiElements()
 
         if (enoughData()) {
             saveData()
-            resetUiElements()
             resetData()
+            resetUiElements()
         }
     }
 
+    private fun getAudioData(arrays: List<ByteArray>) {
+        var rawAudioData = arrays[1]
+
+        while (rawAudioData.size > EnvVariables.DESIRED_LENGTH) {
+            rawAudioData = processor.process(rawAudioData)
+        }
+
+        audioData += rawAudioData
+    }
+
+    private fun getStringData(arrays: List<ByteArray>) {
+        var rawStringData = arrays[0]
+
+        while (rawStringData.size > EnvVariables.DESIRED_LENGTH) {
+            rawStringData = processor.process(rawStringData)
+        }
+
+        stringData += rawStringData
+    }
+
     private fun saveData() {
+        val lists = listOf(
+            audioData.slice(0 until EnvVariables.DESIRED_LENGTH).toByteArray(),
+            stringData.slice(0 until EnvVariables.DESIRED_LENGTH).toByteArray()
+        )
+
+        val xor = ByteArrayListXOR.xor(lists)
+        val result = ByteArrayToBinaryStringConverter.convert(xor)
+
         DataSaver.saveData(
-            data = generatedData,
+            data = result,
             table = "speech"
         )
     }
 
+    private fun enoughStringData() = stringData.size >= EnvVariables.DESIRED_LENGTH
+
+    private fun enoughAudioData() = audioData.size >= EnvVariables.DESIRED_LENGTH
+    
     private fun resetData() {
-        generatedData = ""
+        stringData = ByteArray(0)
+        audioData = ByteArray(0)
     }
 
     private fun resetUiElements() {
@@ -62,9 +105,21 @@ class SpeechToTextActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     private fun updateUiElements() {
-        binding.progressBarSpeech.progress = generatedData.length
+        binding.progressBarSpeech.progress = smallestArraySize()
         val percentage =
             (binding.progressBarSpeech.progress.toFloat() / binding.progressBarSpeech.max.toFloat()) * 100
         binding.txtProgressBarSpeechPercent.text = "${percentage.toInt()}%"
+    }
+
+    private fun smallestArraySize(): Int {
+        val arrays = listOf(
+            audioData,
+            stringData
+        )
+        return arrays.minBy { it.size }.size ?: 0
+    }
+
+    private fun enoughData(): Boolean {
+        return smallestArraySize() >= EnvVariables.DESIRED_LENGTH
     }
 }
--- app/src/main/java/com/example/utilities/ByteArrayProcessor.kt
+++ app/src/main/java/com/example/utilities/ByteArrayProcessor.kt
@@ -1,4 +1,19 @@
 package com.example.utilities
 
+import kotlin.experimental.xor
+
 class ByteArrayProcessor {
+    fun process(input: ByteArray): ByteArray {
+        var byteArray = input
+        if (byteArray.size % 2 != 0) {
+            byteArray = byteArray.copyOfRange(1, byteArray.size)
+        }
+
+        val newByteArray = ByteArray(byteArray.size / 2)
+        for (i in byteArray.indices step 2) {
+            newByteArray[i / 2] = (byteArray[i] xor byteArray[i + 1]).toByte()
+        }
+
+        return newByteArray
+    }
 }
