c2ff2ebfa493ecfe9a3cb1c544ba9060a3ffbda0
Tom Welter <welter.tom@hotmail.com>
2023-02-04 22:46:04 +0100

    Replace random with SecureRandom Restructure of method

--- app/src/main/java/com/example/pseudorandomgenerator/TypingActivity.kt
+++ app/src/main/java/com/example/pseudorandomgenerator/TypingActivity.kt
@@ -6,46 +6,48 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import com.example.converters.ByteArrayToBinaryStringConverter
 import com.example.pseudorandomgenerator.databinding.ActivityTypeingBinding
 import com.example.utilities.*
+import java.security.SecureRandom
+import kotlin.random.Random
 
 class TypingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTypeingBinding
 
     private var keystrokeData = ByteArray(0)
     private var timeData = ByteArray(0)
-
+    private var alphabet: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    private var secRandom: SecureRandom = SecureRandom()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTypeingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.progressBarTimeTyping.max = EnvVariables.DESIRED_LENGTH
-
         initListeners()
         }
 
-    private fun randInput(){
-        val alphabet: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-        binding.editTxtTimeTyping.setText(alphabet.random().toString())
-    }
 
     private fun runTest(){
         for (i in 1..10000){
             for (i in 1..64){
-                randInput()
-                Thread.sleep(80)
+                val randChar : String = alphabet[secRandom.nextInt(alphabet.size)].toString()
+                Log.d("Letter", randChar)
+                binding.editTxtTimeTyping.setText(randChar)
+                Thread.sleep(secRandom.nextInt(40)+40.toLong())
             }
+            if (i %10 ==0) Log.d("ITERATION", i.toString())
         }
     }
 
     private fun initListeners() {
         binding.btnTypingTest.setOnClickListener()
         {
-            Log.d("TEST", "Test run")
+            Log.d("AUTO_DATA", "AUTO_DATA run start")
             runTest()
-            Log.d("TEST", "Test end")
+            Log.d("AUTO_DATA", "AUTO_DATA run end")
         }
         binding.editTxtTimeTyping.addTextChangedListener(/* watcher = */ object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
@@ -85,7 +87,7 @@ class TypingActivity : AppCompatActivity() {
 
         DataSaver.saveData(
             data = string,
-            table = "typing"
+            table = "typing_automated"
         )
 
         DataSaver.saveData(
